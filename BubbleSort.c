15f4d6527ef6d348272c115421f994f0f643e8cb
Salitha Tennakoon <salithaniranjana@gmail.com>
2020-10-03 00:29:35 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,14 +1,13 @@
-#include <studio.h>
-
+#include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
     bool swap;
-    int n=5,j;
+    int n=5,j,temp,m;
     int arr[5]={58,89,71,35,6};
     
-    
-    do{
+   do{
         swap=false;
         for(j=0;j<n;j++)
         {
@@ -16,14 +15,15 @@ int main()
             {
                 temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=temp
+                arr[j+1]=temp;
                 swap=false;
             }
         }
     }while(swap);
-    
-    for(m=0;m<n;m--)
+
+    for(m=0;m<n;m++)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[m]);
         }
 }
+
