850bc8ea18705aaad4e95cfb3893de4e99c0affe
Mandara Sharannya <mandarasharannya990116@gmail.com>
2020-10-05 11:53:39 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,4 +1,5 @@
-#include <studio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 
 int main()
@@ -12,18 +13,20 @@ int main()
         swap=false;
         for(j=0;j<n;j++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[j]>arr[j+1]);
             {
-                temp=arr[j];
+                int temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=temp
+                arr[j+1]=temp;
                 swap=false;
             }
         }
     }while(swap);
     
-    for(m=0;m<n;m--)
+    for(int m=0;m<n;m++)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[m]);
         }
 }
+
+
