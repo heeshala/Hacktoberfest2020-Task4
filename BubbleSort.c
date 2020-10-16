8b2ca1a7057777ce95d1ceb4b9aec401eceed8a1
SanduniNimanthika <42827651+SanduniNimanthika@users.noreply.github.com>
2020-10-16 16:19:54 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -12,18 +12,18 @@ int main()
         swap=false;
         for(j=0;j<n;j++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[j]>arr[j+1]);
             {
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp
-                swap=false;
+                swap=true;
             }
         }
     }while(swap);
     
-    for(m=0;m<n;m--)
+    for(m=0;m<n;m++)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[m]);
         }
 }
