d41720a425b30b1c2407c0efb74527909b2b75b5
Eshan Agarwal <eshan.agarwal9@gmail.com>
2020-10-17 20:37:02 +0400

    Corrected Bubble Sort

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,29 +1,27 @@
-#include <studio.h>
+#include <stdio.h>
 
 
 int main()
 {
-    bool swap;
     int n=5,j;
     int arr[5]={58,89,71,35,6};
+    int temp;
     
     
-    do{
-        swap=false;
-        for(j=0;j<n;j++)
+    for(int i=0 ; i<n-1 ; i++){
+        for(j=1;j<n-i;j++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[j]<arr[j-1])
             {
                 temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
             }
         }
-    }while(swap);
+    }
     
-    for(m=0;m<n;m--)
+    for(int m=0;m<n;m++)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[m]);
         }
 }
