35002531d49287b84ea3015bccb1e58ba34302a0
Hasindu Dahanayake <hasiyadahanayake@gmail.com>
2020-10-21 23:43:05 +0530

    Updated BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,29 +1,42 @@
-#include <studio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 
 int main()
 {
     bool swap;
     int n=5,j;
-    int arr[5]={58,89,71,35,6};
+    int arr[5]={58,89,71,35,6};//Defining array elements
     
-    
+    /*
+     * Start of bubble sort algorithom implementation
+    */
     do{
         swap=false;
-        for(j=0;j<n;j++)
+        
+        for(j=0;j<n-1;j++)//Iterating through the array eleemts
         {
-            if (arr[j]>arr[j-1]);
+            
+            if (arr[j]>arr[j+1])//Swap array elemets if required
             {
-                temp=arr[j];
+                int temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
+                arr[j+1]=temp;
+                swap=true;
             }
         }
+       
+     
     }while(swap);
+    /*
+     * End of bubble sort algorithom implementation
+    */
     
-    for(m=0;m<n;m--)
-        {
-            printf("%s ",arr[l]);
+    for(int m=0;m<n;m++)//Printing Sorted array
+        {  
+            printf("%d",arr[m]);//Print array element
+            if( m < n-1){
+            printf(",");//Foramatting the output
+             }
         }
 }
