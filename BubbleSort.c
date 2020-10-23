f00d1196ff97b7feb9a86d0139881c30045f1c61
chala2019 <52007759+chala2019@users.noreply.github.com>
2020-10-23 13:14:14 +0530

    Update BubbleSort.c
    Made changes and updated the code snippet with ideal fixings.

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,11 +1,21 @@
 #include <studio.h>
 
-
 int main()
 {
+     int number1, number2, sum;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2); // defines the inputs
+
+    // calculating sum
+    sum = number1 + number2;      
+
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0; // return statement
+
     bool swap;
     int n=5,j;
-    int arr[5]={58,89,71,35,6};
+    int arr[5]={58,89,71,35,6}; // arguements
     
     
     do{
@@ -15,15 +25,15 @@ int main()
             if (arr[j]>arr[j-1]);
             {
                 temp=arr[j];
-                arr[j]=arr[j+1];
+                arr[j]=arr[j+1]; // logic defining
                 arr[j+1]=temp
                 swap=false;
             }
         }
-    }while(swap);
+    }while(swap); // swap
     
     for(m=0;m<n;m--)
         {
-            printf("%s ",arr[l]);
+            printf("%s ",arr[l]); // final statement
         }
 }
