3c3af419b760d361dcf82b47d1dd33004056f44b
shaku237 <42364827+shaku237@users.noreply.github.com>
2020-10-23 12:27:16 +0530

    Updated the code

--- BubbleSort.c
+++ BubbleSort.c
@@ -5,11 +5,11 @@ int main()
 {
     bool swap;
     int n=5,j;
-    int arr[5]={58,89,71,35,6};
+    int arr[5] = {58,89,71,35,6};
     
     
     do{
-        swap=false;
+        swap = false;
         for(j=0;j<n;j++)
         {
             if (arr[j]>arr[j-1]);
@@ -24,6 +24,6 @@ int main()
     
     for(m=0;m<n;m--)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[l]);
         }
 }
