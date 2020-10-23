1431f1a2d9294b72c3c07d3d8a93f61e544988b7
Tharu1999 <52005233+Tharu1999@users.noreply.github.com>
2020-10-23 12:09:18 +0530

    UpdateBubble-tharu.c
    Arranged the bubble sort in a different way of implementing.

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,11 +1,21 @@
 #include <studio.h>
 
-
 int main()
 {
+     int number1, number2, sum;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2); // user input
+
+    // calculating sum
+    sum = number1 + number2;      // calculation
+
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0; // return statement
+
     bool swap;
     int n=5,j;
-    int arr[5]={58,89,71,35,6};
+    int arr[5]={58,89,71,35,6}; // defines
     
     
     do{
@@ -15,7 +25,7 @@ int main()
             if (arr[j]>arr[j-1]);
             {
                 temp=arr[j];
-                arr[j]=arr[j+1];
+                arr[j]=arr[j+1]; // logic implementing
                 arr[j+1]=temp
                 swap=false;
             }
@@ -24,6 +34,6 @@ int main()
     
     for(m=0;m<n;m--)
         {
-            printf("%s ",arr[l]);
+            printf("%s ",arr[l]); // final statement
         }
 }
